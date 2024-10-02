a9032288be5b9a13493a4da86843aebc02b37e9a
hirojinho <eduardo.haraguchi@gmail.com>
2024-10-02 00:10:44 -0300

    feat: server to update the text

---
+++ ConcurrencyBackend/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(req *http.Request) bool {
+		return true // Allow all origins
+	},
+}
+
+func handleConnection(writer http.ResponseWriter, req *http.Request) {
+	// Upgrade the HTTP connection to a WebSocket connection
+	conn, readErr := upgrader.Upgrade(writer, req, nil)
+	if readErr != nil {
+		log.Println("Error upgrading connection:", readErr)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("New connection established, client connected")
+
+	for {
+		// Read message from client
+		var messageType int
+		var message []byte
+		var err error
+
+		messageType, message, err = conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+
+		var writerErr error
+		// Echo the message back to the client
+		if writerErr = conn.WriteMessage(messageType, message); writerErr != nil {
+			log.Println("Error writing message:", writerErr)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnection) // Set up the WebSocket endpoint
+
+	fmt.Println("Starting server on :8080")
+	var err error
+	if err = http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Error starting server:", err)
+	}
+}
